587d537e901c1ac95e1770c808e17d3758196bbe
Simon <superpenshine@gmail.com>
2017-10-07 19:33:19 -0700

    v1.32 js update

--- rip/static/javascript/courses.js
+++ rip/static/javascript/courses.js
@@ -1,8 +1,8 @@
-max_length = 500;
+max_length = 250;
 
-function checkWord(c){
-	//realLength= c.value.length;
-	realLength= getStrleng(c.value);
+function checkWord(o){
+	realLength= o.value.length;
+	//realLength= getStrleng(o.value);
 	var wck = document.getElementById("wordCheck");
 	console.log(realLength);
 	//non-english characters
@@ -10,18 +10,21 @@ function checkWord(c){
 	wck.innerHTML = max_length - realLength;
 
 }
-
 //calculate string length
-function getStrleng(str){
-
-	myLen = 0;
-
-	for(i = 0;i < str.length;i++){
-		if(str.charCodeAt(i) > 0 && str.charCodeAt(i) < 128)
-			myLen++;
-		else
-			myLen+=2;
+// function getStrleng(str){
+// 	myLen = 0
+// 	for(i = 0;i < str.length;i++){
+// 		if(str.charCodeAt(i) > 0 && str.charCodeAt(i) < 128)
+// 			myLen++;
+// 		else
+// 			myLen+=2;
+// 	}
+// 	return myLen;
+// }
+//prevent paste from excessing word limits
+function prevent_paste(o){
+	text_length = o.value.length;
+	if(text_length > max_length){
+		num = o.value.substring(0, max_length);
 	}
-
-	return myLen;
 }
