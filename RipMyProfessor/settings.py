9b3f78e53ad4f69fce06a6524bfb6b3c63fb1eff
Simon <superpenshine@gmail.com>
2017-10-13 18:12:23 -0700

    whitenoise middleware added

--- RipMyProfessor/settings.py
+++ RipMyProfessor/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
 	'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',#whitenoise used in heroku
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
