f861a3e235df08c6a39f15bc305c232def3cfa1d
Simon <superpenshine@gmail.com>
2017-10-23 12:41:04 -0700

    V2.61google analytics

--- RipMyProfessor/settings/prod.py
+++ RipMyProfessor/settings/prod.py
@@ -84,6 +84,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 # CACHES = {
 #     "default": {
 #         "BACKEND": "django.core.cache.backends.db.DatabaseCache",
